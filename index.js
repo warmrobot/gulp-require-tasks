38f0bf1ef3165e7fadd596625d09f01c064e260f
Xavier Zalawa <xavier@7studio.fr>
2017-05-09 21:50:10 +0200

    Register task with the same name than directory
    By adding an `index.js` into a directory you are now allowed to register
    a task with the same name than the directory.

--- index.js
+++ index.js
@@ -75,24 +75,21 @@ function gulpRequireTasks (options) {
      */
     function taskNameFromPath (modulePath) {
       const relativePath = path.relative(options.path, modulePath);
-      return removeExtension(relativePath)
-        .split(path.sep).join(options.separator)
-      ;
+      const pathInfos = path.parse(relativePath);
+      var taskNameParts = []
+
+      if (pathInfos.dir) {
+        taskNameParts.push(...pathInfos.dir.split(path.sep));
+      }
+      if ('index' != pathInfos.name) {
+        taskNameParts.push(pathInfos.name);
+      }
+
+      return taskNameParts.join(options.separator);
     }
 
   }
 
-}
-
-/**
- * Removes extension from the specified path.
- *
- * @param {string} path
- *
- * @returns {string}
- */
-function removeExtension (path) {
-  return path.substr(0, path.lastIndexOf('.'))
 }
 
 /**
